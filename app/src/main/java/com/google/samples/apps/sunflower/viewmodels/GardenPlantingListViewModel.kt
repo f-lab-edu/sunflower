0dfdc2382388e21e7f9890ac2086f487b7e4be44
parkcheayeon <ddaany927@gmail.com>
2024-12-14 21:02:56 +0900

    GardenPlantingListViewModel 분석

--- app/src/main/java/com/google/samples/apps/sunflower/viewmodels/GardenPlantingListViewModel.kt
+++ app/src/main/java/com/google/samples/apps/sunflower/viewmodels/GardenPlantingListViewModel.kt
@@ -16,6 +16,9 @@
 
 package com.google.samples.apps.sunflower.viewmodels
 
+// [GardenPlantingRepository를 통해 정원에 심은 식물들 정보를 가져와서 LiveData로 변환한다]
+// 변환해서 UI에 어떻게 전달, 반영한다는 걸까? GardenFragment 확인 필요
+
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
@@ -29,16 +32,37 @@ import javax.inject.Inject
 // ViewModel에서 필요한 데이터나 기능을 쉽게 가져오도록 해준다
 // ViewModel에 필요한 의존성을 간편하게 주입받을 수 있으며, 생명 주기를 관리하여 메모리 누수를 방지
 
+// ViewModel 이란? Activity나 Fragment가 Destroy 또는 구성변경에도 데이터를 유지하도록 도와준다
+// 직접 데이터를 가져오거나 저장하지 않고, Repository를 통해 데이터 작업을 요청한다.
+
 // @Inject => GardenPlantingRepository를 매개변수로 받아서 사용할 수 있게 한다
 // internal => 외부에서 이 ViewModel을 생성하지 못하게 한다
 // constructor => 클래스의 생성자, 지우면 에러발생
 // 어노테이션이 생성자에 적용될 때는 constructor를 명시해야 한다
-class GardenPlantingListViewModel @Inject internal constructor(
-    gardenPlantingRepository: GardenPlantingRepository // repository 확인 필요
-) : ViewModel() { // GardenPlantingListViewModel이 ViewModel을 상속받음
-    // LiveData => 데이터가 변경되면 자동으로 감지하여, Fragment를 업데이트
+// Hilt가 GardenPlantingRepository 객체를 생성하고 ViewModel에 전달
+class GardenPlantingListViewModel @Inject internal constructor(gardenPlantingRepository: GardenPlantingRepository) : ViewModel() {
+    // private로 선언하지 않고 internal로 한 이유는?
+    // ViewModel은 주로 같은 모듈 내의 Activity나 Fragment에서 사용된다
+    // 같은 모듈 내 테스트 코드에서는 접근 가능해야 한다
+    // GardenPlantingListViewModel이 ViewModel을 상속받음
+
     val plantAndGardenPlantings: LiveData<List<PlantAndGardenPlantings>> =
+        // PlantAndGardenPlantings 객체들을 List 형태로 반환
+
+        // LiveData란 뭘까? Observer 클래스
+        // Activity, Fragment와 같은 UI 컴포넌트의 생명주기를 인식해서 UI가 onStop, onDestroy일 때는 관찰자에게 데이터를 전달하지 않는다
+        // 데이터가 변경되면 자동으로 Observer에게 알림
+        // UI가 재생성되어도 마지막 상태의 데이터를 보존한다
+
+        // 왜 이번엔 Flow가 아니라 LiveData를 사용하는 걸까?
+        // Flow는 UI 생명주기 관리 기능이 없으므로, 데이터를 관찰하는 UI와 직접 연결하기에는 부적합하다
+
         gardenPlantingRepository.getPlantedGardens().asLiveData()
+        // asLiveData() 란? Flow로 반환되는 데이터를 LiveData로 변환한다
+        // 따라서 UI 생명주기를 자동으로 인식하고 데이터를 안전하게 전달한다
+
+        // Flow란 비동기라고 했는데 코루틴이랑 연관이 있나? Flow는 코루틴 내부에서 실행된다
+        // 그렇다면 개발자가 관여해야할 부분이 있을까? Room과 결합된 Flow는 DB 작업과 변경 사항 감지를 자동화한다
 }
 
 //class GardenPlantingListViewModel : ViewModel() {
